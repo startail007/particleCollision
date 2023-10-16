9205f62d9527fdd146f862e544d70da49018187b
Cai <flashmx29403229@gmail.com>
2023-10-16 09:02:08 +0800

    更新

--- src/fluid/index.js
+++ src/fluid/index.js
@@ -5,6 +5,7 @@ import { Float } from "../js/Float.js";
 class FluidParticle {
   constructor(pos) {
     this.pos = Vector.clone(pos);
+    this.predictedPos = Vector.zero();
     this.force = Vector.zero();
     this.linearVel = Vector.zero();
     this.active = false;
@@ -23,19 +24,19 @@ class FluidParticle {
 
   render(ctx) {
     //ctx.fillStyle = this.active ? "#ff0000" : "#ffffff";
-    const t = Float.clamp(0, 1, Float.toRate(0.01, 0.025, this.fieldDensity));
-    // console.log(this.fieldDensity);
+    // const t = Float.clamp(0, 1, Float.toRate(0.01, 0.025, this.fieldDensity));
+    const t = Float.clamp(0, 1, Float.toRate(0, 600, Vector.length(this.linearVel)));
     const h = Math.floor(Float.mix(0, 360, t));
     ctx.fillStyle = `hsl(${h},100%,50%)`;
     ctx.beginPath();
     ctx.arc(...this.pos, this.radius, 0, 2 * Math.PI);
     ctx.fill();
 
-    ctx.strokeStyle = "#ffffff";
-    ctx.beginPath();
-    ctx.moveTo(...this.pos);
-    ctx.lineTo(...Vector.add(this.pos, Vector.scale(this.propertyForce, 0.02)));
-    ctx.stroke();
+    // ctx.strokeStyle = "#ffffff";
+    // ctx.beginPath();
+    // ctx.moveTo(...this.pos);
+    // ctx.lineTo(...Vector.add(this.pos, Vector.scale(this.linearVel, 1 * 0.02)));
+    // ctx.stroke();
   }
 }
 const canvas = document.getElementById("canvas");
@@ -46,14 +47,14 @@ const cHeight = canvas.height;
 const rect = new Rectangle(0, 0, cWidth, cHeight);
 const qtree = new Quadtree(rect, 10);
 const particles = [];
-const particleCount = 900;
+const particleCount = 400;
 const collisionDamping = 1;
-const gravity = [0, 9.81 * 100];
-const w = 30;
+const gravity = [0, 9.81 * 10];
+const w = 20;
 const h = Math.ceil(particleCount / w);
-const spacing = 10;
+const spacing = 8;
 for (let i = 0; i < particleCount; i++) {
-  // particles[i] = new FluidParticle([Math.random() * cWidth, Math.random() * cHeight]);
+  //particles[i] = new FluidParticle([Math.random() * cWidth, Math.random() * cHeight]);
   // particles[i].linearVel = [200 - Math.random() * 400, 200 - Math.random() * 400];
   const x = i % w;
   const y = Math.floor(i / w);
@@ -61,7 +62,20 @@ for (let i = 0; i < particleCount; i++) {
     new FluidParticle([cWidth * 0.5 + (x - w * 0.5 + 0.5) * spacing, cHeight * 0.5 + (y - h * 0.5 + 0.5) * spacing])
   );
 }
-canvas.addEventListener("click", (ev) => {});
+canvas.addEventListener("click", (ev) => {
+  VectorE.set(mPos, [ev.offsetX, ev.offsetY]);
+  const particleCount = 100;
+  const w = 10;
+  const h = Math.ceil(particleCount / w);
+  const spacing = 10;
+  for (let i = 0; i < particleCount; i++) {
+    const x = i % w;
+    const y = Math.floor(i / w);
+    particles.push(
+      new FluidParticle([mPos[0] + (x - w * 0.5 + 0.5) * spacing, mPos[1] + (y - h * 0.5 + 0.5) * spacing])
+    );
+  }
+});
 const mPos = [0, 0];
 canvas.addEventListener("mousemove", (ev) => {
   VectorE.set(mPos, [ev.offsetX, ev.offsetY]);
@@ -93,12 +107,12 @@ const smoothingKernel = (radius, dist) => {
   const volume = (Math.PI * radius ** 4) / 6;
   return (radius - dist) ** 2 / volume;
 };
-const calcDensity = (particleA, particles, smoothingRadius) => {
+const calcDensity = (pos, particles, smoothingRadius) => {
   let density = 0;
   const mass = 1;
   particles.forEach((particle) => {
-    const dist = Vector.distance(particle.pos, particleA.pos);
-    const influence = smoothingKernel(smoothingRadius / 100, dist / 100);
+    const dist = Vector.distance(particle.pos, pos);
+    const influence = smoothingKernel(smoothingRadius, dist);
     density += mass * influence;
   });
   return density;
@@ -118,28 +132,29 @@ const getRandomDir = () => {
   const angle = Math.random() * 2 * Math.PI;
   return [Math.cos(angle), Math.sin(angle)];
 };
+const targetDensity = 0.012;
+const pressureMul = 40;
 const convertDensityToPressure = (density) => {
-  return density * 9.81 * 100;
+  //return density * 9.81 * 100;
+  const densityError = density - targetDensity;
+  const pressure = densityError * pressureMul;
+  return pressure;
 };
 const calcPropertyForce = (particleA, particles, smoothingRadius) => {
   const propertyForce = Vector.zero();
-  const mass = 0.01;
+  const mass = smoothingRadius * smoothingRadius * Math.PI;
   particles.forEach((particle) => {
     if (particle === particleA) return;
     const v = Vector.sub(particle.pos, particleA.pos);
     const dist = Vector.length(v);
     if (!dist) return;
     const dir = dist == 0 ? getRandomDir() : Vector.divScale(v, dist);
-    const slop = smoothingKernelDerivative(dist / 100, smoothingRadius / 100);
+    const slop = smoothingKernelDerivative(dist, smoothingRadius);
     let density = particle.fieldDensity;
     if (!density) return;
-    const d = Float.mix(convertDensityToPressure(density), convertDensityToPressure(particleA.fieldDensity), 0.5);
+    const d = (convertDensityToPressure(density) + convertDensityToPressure(particleA.fieldDensity)) * 0.5;
     VectorE.add(propertyForce, Vector.scale(dir, (d * slop * mass) / density));
   });
-  // const len = Vector.length(propertyForce);
-  // if (len > 100) {
-  //   VectorE.scale(propertyForce, 100 / len);
-  // }
   return propertyForce;
 };
 const update = (delta) => {
@@ -150,7 +165,7 @@ const update = (delta) => {
     particle.active = false;
     qtree.insert({ key: i, point: particle.pos });
   });
-  const radius = 50;
+  const radius = 30;
 
   const list = particles.map((particle, i) => {
     const range = new Rectangle(particle.pos[0] - radius, particle.pos[1] - radius, radius * 2, radius * 2);
@@ -159,21 +174,24 @@ const update = (delta) => {
       .filter((el) => Vector.distance(particles[el.key].pos, particle.pos) <= radius)
       .map((el) => particles[el.key]);
   });
-  list.forEach((query_particles, i) => {
-    particles[i].fieldDensity = calcDensity(particles[i], query_particles, radius);
+  particles.forEach((particle) => {
+    VectorE.add(particle.linearVel, Vector.scale(gravity, delta));
+    VectorE.set(particle.predictedPos, Vector.add(particle.pos, Vector.scale(particle.linearVel, delta)));
   });
   list.forEach((query_particles, i) => {
-    particles[i].propertyForce = calcPropertyForce(particles[i], query_particles, radius);
+    particles[i].fieldDensity = calcDensity(particles[i].predictedPos, query_particles, radius);
+  });
+  list.forEach((query_particles, i) => {
+    const particle = particles[i];
+    VectorE.scale(particle.linearVel, 0.99);
+    const propertyForce = calcPropertyForce(particle, query_particles, radius);
+    const propertyAcc = Vector.divScale(propertyForce, particle.fieldDensity);
+    VectorE.add(particle.linearVel, Vector.scale(propertyAcc, delta));
   });
 
   particles.forEach((particle) => {
-    //particle.force = gravity;
-    VectorE.scale(particle.linearVel, 0.999);
-    const propertyAcc = Vector.divScale(particle.propertyForce, particle.fieldDensity);
-    VectorE.add(particle.linearVel, Vector.scale(propertyAcc, delta));
-    // VectorE.add(particle.linearVel, propertyAcc);
-    // VectorE.add(particle.linearVel, propertyAcc);
-    particle.update(delta);
+    // particle.update(delta);
+    VectorE.add(particle.pos, Vector.scale(particle.linearVel, delta));
   });
 
   particles.forEach((particle) => resolveCollision(particle, rect, collisionDamping));
